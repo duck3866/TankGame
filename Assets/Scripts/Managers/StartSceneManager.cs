137cff87efb900a3a68fbc9cc1dd09ce8315a200
duck3866 <130462482+duck3866@users.noreply.github.com>
2025-03-07 14:10:47 +0900

    Feat :: UI 시작 애니메이션 추가

---
+++ Assets/Scripts/Managers/StartSceneManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : MonoBehaviour
+{
+    [SerializeField] private string _playSceneName;
+    [SerializeField] private GameObject _settingPanel;
+    [SerializeField] private Animator _settingPanelAnimator;
+
+
+    public void Start()
+    {
+        // _settingPanelAnimator.SetTrigger("Start");
+    }
+    /// <summary>
+    /// 시작 버튼 클릭 함수
+    /// </summary>
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene(_playSceneName);
+    }
+    /// <summary>
+    /// 종료 버튼 클릭 함수
+    /// </summary>
+    public void OnClickExitButton()
+    {
+        Application.Quit();
+    }
+    /// <summary>
+    /// 설정 창을 비활성화하는 함수
+    /// </summary>
+    public void OnCloseSettingButton()
+    {
+        _settingPanel.SetActive(false);
+    }
+    /// <summary>
+    /// 설정 창을 활성화하는 함수
+    /// </summary>
+    public void OnOpenSettingButton()
+    {
+        _settingPanel.SetActive(true);
+    }
+}
