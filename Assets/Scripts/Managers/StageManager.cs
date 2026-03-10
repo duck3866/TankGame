70f43dff9f430712d7186a1cf9efcabb96c8a6d2
duck3866 <a44540159a@gmail.com>
2026-03-11 00:08:28 +0900

    fix: Debug 삭제

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -38,7 +38,6 @@ public class StageManager : MonoBehaviour
     public void OnStageButtonClicked(int clickIndex)
     {
         index = clickIndex;
-        Debug.Log(index+" 와우 섹123스");
         SceneManager.LoadScene("Main");
     }
 }
